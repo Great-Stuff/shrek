b4c33f04c58e6707bf950a084074ff439526ff1a
Great-Stuff <124748279+Great-Stuff@users.noreply.github.com>
2023-09-03 18:10:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
+const img_wrapper = document.querySelector(".img-wrapper")
+const image_count = document.querySelector(".image-count")
+var count_image = 0
+
+function count(){
+    count_image = img_wrapper.children.length + 1
+    image_count.textContent = count_image.toString()
+}
+
 document.addEventListener("click", (e) => {
     const percent = Math.round(Math.random()*100)
 
-    if (percent <= 4){
+    if(percent <= 1){
+        var img = "images/taylor.png"
+    }
+    else if (percent <= 5){
         var img = "images/weird-shrek-4.png"
+        count()
     }
     else if (percent <= 24){
         var img = "images/silly-shrek-36.png"
+        count()
     }
     else if (percent <= 60){
         var img = "images/shrek-bathroom-20.png"
+        count()
     }
     else{
         var img = "images/shrek-40.png"
+        count()
+
     }
 
     const DOM_img = new Image()
     DOM_img.src = img
     DOM_img.draggable = false
-    document.body.appendChild(DOM_img)
+    img_wrapper.appendChild(DOM_img)
 
     const posX = e.clientX - (DOM_img.offsetWidth/2)
     const posY = e.clientY- (DOM_img.offsetHeight/2)
@@ -25,6 +42,9 @@ document.addEventListener("click", (e) => {
     DOM_img.style = `left:${posX}px; top:${posY}px;`
 
     if (e.target.classList.contains("clear")) {
-        document.body.innerHTML = "<button class='clear'>Clear</button>";
+        while (img_wrapper.hasChildNodes()){
+            img_wrapper.firstChild.remove()
+        }
+        image_count.textContent = "0"
     }
 })
