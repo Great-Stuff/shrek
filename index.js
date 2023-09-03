4423f4632c98e08eaa0dd74dbdf68dcaa65a274c
Great-Stuff <124748279+Great-Stuff@users.noreply.github.com>
2023-09-03 17:28:43 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-document.addEventListener("mouseup", (e) => {
+document.addEventListener("click", (e) => {
     const percent = Math.round(Math.random()*100)
-    const posX = e.clientX
-    const posY = e.clientY
 
     if (percent <= 4){
-        document.body.innerHTML += `<img src='images/weird-shrek-4.png' draggable='false' style='left:${posX}px; top:${posY}px;'>`
+        var img = "images/weird-shrek-4.png"
     }
     else if (percent <= 24){
-        document.body.innerHTML += `<img src='images/silly-shrek-36.png' draggable='false' style='left:${posX}px; top:${posY}px;'>`
+        var img = "images/silly-shrek-36.png"
     }
     else if (percent <= 60){
-        document.body.innerHTML += `<img src='images/shrek-bathroom-20.png' draggable='false' style='left:${posX}px; top:${posY}px;'>`
+        var img = "images/shrek-bathroom-20.png"
     }
     else{
-        document.body.innerHTML += `<img src='images/shrek-40.png' draggable='false' style='left:${posX}px; top:${posY}px;'>`
+        var img = "images/shrek-40.png"
     }
+
+    const DOM_img = new Image()
+    DOM_img.src = img
+    DOM_img.draggable = false
+    document.body.appendChild(DOM_img)
+
+    const posX = e.clientX - (DOM_img.offsetWidth/2)
+    const posY = e.clientY- (DOM_img.offsetHeight/2)
+
+    DOM_img.style = `left:${posX}px; top:${posY}px;`
 
     if (e.target.classList.contains("clear")) {
         document.body.innerHTML = "<button class='clear'>Clear</button>";
